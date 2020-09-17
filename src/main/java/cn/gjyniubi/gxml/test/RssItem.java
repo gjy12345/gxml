532a740891cad6b2167430c144df0dcc4528e372
root <956115488@qq.com>
2020-09-17 15:12:28 +0800

    修复bug，子元素xpath相对路径不需要/开头

--- src/main/java/cn/gjyniubi/gxml/test/RssItem.java
+++ src/main/java/cn/gjyniubi/gxml/test/RssItem.java
@@ -1,21 +1,106 @@
 package cn.gjyniubi.gxml.test;
 
 public class RssItem {
-    @XmlRule("/title")
+    @XmlRule("title")
     private String title;
-    @XmlRule("/link")
+    @XmlRule("link")
     private String link;
-    @XmlRule("/description")
+    @XmlRule("description")
     private String description;
-    @XmlRule("/dc:creator")
+    @XmlRule("dc:creator")
     private String creator;
-    @XmlRule("/pubDate")
+    @XmlRule("pubDate")
     private String pubDate;
-    @XmlRule("/guid")
+    @XmlRule("guid")
     private String guid;
-    @XmlRule("")
-    private TestInner testInner;
+    @XmlRule("category")
+    private String category;//分类
+    @XmlRule("thumb")
+    private String thumb;
+    @XmlRule("tag")
+    private String tag;
+    @XmlRule("author")
+    private String author;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getThumb() {
+        return thumb;
+    }
+
+    public void setThumb(String thumb) {
+        this.thumb = thumb;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
     @Override
     public String toString() {
@@ -26,19 +111,7 @@ public class RssItem {
                 ", creator='" + creator + '\'' +
                 ", pubDate='" + pubDate + '\'' +
                 ", guid='" + guid + '\'' +
-                ", testInner=" + testInner +
                 '}';
     }
 
-    public static class TestInner{
-        @XmlRule("/guid")
-        private String test;
-
-        @Override
-        public String toString() {
-            return "TestInner{" +
-                    "test='" + test + '\'' +
-                    '}';
-        }
-    }
 }
