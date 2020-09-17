d00aeee03fb65b387d59a6769070490cf81d6f12
root <956115488@qq.com>
2020-09-17 15:14:31 +0800

    增加网络加载url测试

--- src/main/java/cn/gjyniubi/gxml/test/Test.java
+++ src/main/java/cn/gjyniubi/gxml/test/Test.java
@@ -6,7 +6,8 @@ import org.dom4j.Document;
 public class Test {
 
     public static void main(String[] args) throws Exception{
-        Document document= Dom4jTool.parseXmlByFile(Test.class.getClassLoader().getResource("rss.xml").getFile());
+        //Document document= Dom4jTool.parseXmlByFile(Test.class.getClassLoader().getResource("rss.xml").getFile());
+        Document document=Dom4jTool.parseXmlByUrl("https://www.gcores.com/rss");
         RssDocument rssDocument=XmlParseEngine.parseXmlToObject(RssDocument.class,document);
         System.out.println(rssDocument);
     }
